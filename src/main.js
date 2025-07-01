57ed8c39282cab83988192e428749c900b10bf7e
Gabriel Alves <gabriel52.alves@gmail.com>
2025-06-30 22:30:02 -0300

    feat: set up Vue application with Vuetify and theme management

--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
+import router from "./router";
+import vuetify from "./plugins/vuetify"; // Certifique-se de que o Vuetify está importado corretamente
 
-createApp(App).mount('#app')
+import "./assets/css/variables.css";
+import "./assets/css/main.css";
+
+import { useThemeStore } from "./store/themeStore";
+
+const app = createApp(App);
+const pinia = createPinia();
+
+app.use(pinia);
+app.use(router);
+app.use(vuetify);
+
+app.mount("#app");
+
+const themeStore = useThemeStore();
+themeStore.applyTheme();
