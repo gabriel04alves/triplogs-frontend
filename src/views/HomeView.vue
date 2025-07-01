f606aff8f0491cb918c6aed2625fafb2b050d290
Gabriel Alves <gabriel52.alves@gmail.com>
2025-06-30 22:41:31 -0300

    feat: enhance responsiveness of HomeView layout for mobile devices

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,14 +3,20 @@
         <v-icon class="px-16 pt-10" color="white" size="50">mdi-compass</v-icon>
         <!-- <HeaderComponent /> -->
         <div class="d-flex fill-height">
-            <div class="d-flex justify-space-between align-end w-100 pa-16 mb-16">
+            <div class="d-flex  align-end w-100 pa-16 mb-16"
+                :class="isMobile ? 'flex-column justify-center ga-16' : 'flex-row justify-space-between'">
                 <div>
-                    <h1 class="text-white text-h2 font-weight-medium">Registre suas novas aventuras</h1>
-                    <h2 class="text-grey-lighten-1 text-h4 font-weight-light">Planeje viagens, explore destinos e salve
+                    <h1 class="text-white text-h2 font-weight-medium"
+                        :class="isMobile ? 'align-self-center text-center' : ''">Registre
+                        suas
+                        novas aventuras</h1>
+                    <h2 class="text-grey-lighten-1 text-h4 font-weight-light" :class="isMobile ? 'd-none' : ''"> Planeje
+                        viagens, explore destinos e salve
                         experiências
                         inesquecíveis.</h2>
                 </div>
-                <v-btn class="rounded-xl text-none text-h5 font-weight-medium" color="var(--color-accent-light)"
+                <v-btn v - btn class=" rounded-xl text-none text-h5 font-weight-medium"
+                    :class="isMobile ? 'align-self-center' : ''" color="var(--color-accent-light)"
                     size="large">Começar</v-btn>
             </div>
         </div>
@@ -20,6 +26,10 @@
 
 <script setup>
 import HeaderComponent from '../components/HeaderComponent.vue';
+import { useDisplay } from 'vuetify';
+
+const { mobile } = useDisplay();
+const isMobile = mobile;
 
 </script>
 
